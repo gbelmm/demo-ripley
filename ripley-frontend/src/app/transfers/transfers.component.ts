656ed39179002fd57c4136876bb481e109fa0313
gbelmm <gbelmm@MacBook-Pro-de-gbelmm.local>
2021-04-08 13:56:51 -0400

    cambios en diseño, agregar icono

--- ripley-frontend/src/app/transfers/transfers.component.ts
+++ ripley-frontend/src/app/transfers/transfers.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TransfersService } from '../api/transfers.service';
-
+import { BanksService } from '../api/banks.service';
 
 export interface Trasnfers {
   name: string;
@@ -23,10 +23,27 @@ const ELEMENT_DATA: Trasnfers[] = [
 export class TransfersComponent implements OnInit {
   displayedColumns: string[] = ['name','rut','bank','type','amount','date'];
   dataSource:any = ELEMENT_DATA;
-  constructor(private transfersService:TransfersService) { }
+  banks: any = [];
+  loading: boolean = false;
+  constructor(private banksService: BanksService,private transfersService:TransfersService) { }
 
   async ngOnInit() {
-   this.dataSource = await this.transfersService.getAll();
+    this.loading = true;
+  
+   let banks: any = await this.banksService.getBanks();
+  
+    this.banks = banks.banks;
+    this.dataSource = await this.transfersService.getAll();
+    for (let index = 0; index < this.dataSource.length; index++) {
+       for (let index2 = 0; index2 < this.banks.length; index2++) {
+         if (this.banks[index2].id==this.dataSource[index].recipient.bank){
+          this.dataSource[index].recipient.bank = this.banks[index2].name
+         }
+         
+       }
+      
+    }
+    this.loading = false;
   }
 
 }
